3de717fcd44bd5fc7e8948542a3729a37935df41
clever_chinekwe <chinekwe_clever@outlook.com>
2023-11-30 16:10:57 +0100

    react app scaffold for animated sidebar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,6 @@
+import { Sidebar } from './Sidebar'
+import { FiGithub } from 'react-icons/fi'
+
+export const Navigation = () => {
+  return <nav>Navbar</nav>
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import { useRef, useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useClickAway } from 'react-use'
+import { AiOutlineRollback } from 'react-icons/ai'
+import { BiHomeSmile, BiUser } from 'react-icons/bi'
+import { HiOutlineChatBubbleBottomCenterText } from 'react-icons/hi2'
+import { FiSettings, FiShoppingCart } from 'react-icons/fi'
+
+export const Sidebar = () => {
+  return <div>Sidebar</div>
+}
